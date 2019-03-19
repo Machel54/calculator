8f77e8d9682a730d513c444209d46526d0688aed
Machel Nyanumba <machelnyanumba14@gmail.comgit config --global user.email machelnyanumba14@gmail.com>
2019-03-19 10:51:40 +0300

    if

--- js/scripts.js
+++ js/scripts.js
@@ -3,3 +3,9 @@ var number1 = parseInt(prompt("Enter a number:"));
 var number2 = parseInt(prompt("Enter another number:"));
 var result = add(number1, number2);
 alert(result);
+var numbers = true;
+if (evennumbers) {
+  console.log("list all of them!");
+} else {
+  console.log("list oddnumbers.");
+}
